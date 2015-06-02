c01ad67fa777241d873634a0115e4db4a821cb64
guosheng <guosheng@lamahui.com>
2015-06-02 17:28:14 +0800

    fix
    fix

--- doc/js/goble.js
+++ doc/js/goble.js
@@ -16,9 +16,15 @@ $(function(){
      $('.editor-html').each(function(){
         var myEditor = $(this)[0];
         var editor = ace.edit(myEditor);
+        editor.session.setMode("ace/mode/html");
         editor.setTheme("ace/theme/monokai");
-        editor.getSession().setMode("ace/mode/html");
-        editor.getSession().setUseWrapMode(true);
+        // editor.getSession().setUseWrapMode(true);
+        // 
+        editor.setOptions({
+            enableBasicAutocompletion: true,
+            enableSnippets: true,
+            enableLiveAutocompletion: false
+        });
     });
     
 });
