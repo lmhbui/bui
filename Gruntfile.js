c183a1a701702c3ead0eb1beed1d50a2157da932
guosheng <guosheng@lamahui.com>
2015-06-02 15:47:05 +0800

    add
    add

---
+++ Gruntfile.js
@@ -0,0 +1,47 @@
+module.exports = function(grunt) {
+    // 项目配置
+    grunt.initConfig({
+        watch: {
+            scripts: {
+                files: 'src/**/*.less',
+                tasks: ['less', 'cssmin'],
+                options: {
+                    interrupt: true,
+                },
+            },
+        },
+        less: {
+            production: {
+                options: {
+                    modifyVars: {
+                        imgPath: '"http://mycdn.com/path/to/images"',
+                        bgColor: 'red'
+                    }
+                },
+                files: {
+                    "dist/lmh.bui.css": "src/*.less"
+                }
+            }
+        },
+
+        cssmin: {
+          options: {
+            shorthandCompacting: false,
+            roundingPrecision: -1
+          },
+          target: {
+            files: {
+              'dist/lmh.bui.min.css': ['dist/lmh.bui.css']
+            }
+          }
+        }
+
+
+
+    });
+
+    grunt.loadTasks('./node_modules/grunt-contrib-watch/tasks');
+     grunt.loadTasks('./node_modules/grunt-contrib-cssmin/tasks');
+    grunt.loadTasks('./node_modules/grunt-contrib-less/tasks');
+    grunt.registerTask('default', ['watch']);
+};
